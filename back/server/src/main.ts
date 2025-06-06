871607c8b097fd803979861efd7d8942dddf4a6b
최경영 <yeong7098@naver.com>
2025-06-07 04:13:23 +0900

    refactor : 도커파일 수정

--- back/server/src/main.ts
+++ back/server/src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser()); 
-  await app.listen(process.env.PORT ?? 8000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: true, 
+    credentials: true, 
+  });
+
+  await app.listen(8000);
 }
 bootstrap();
