5e9d3aec1c306a0c1104692ae438e687de309568
최경영 <yeong7098@naver.com>
2025-06-10 01:27:37 +0900

    refactor : 인증 시간 10분에서 2분으로 수정

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -61,7 +61,7 @@ export default function HomePage() {
     setError("")
     setSuccessMessage("")
     setTimerActive(false)
-    setTimeRemaining(600)
+    setTimeRemaining(120)
   }
 
   const handleSendVerification = async () => {
@@ -92,7 +92,7 @@ export default function HomePage() {
       if (response.status >= 200 && response.status < 300) {
         setIsCodeSent(true)
         setSuccessMessage("인증 코드가 전송되었습니다. 이메일을 확인해주세요.")
-        setTimeRemaining(600)
+        setTimeRemaining(120)
         setTimerActive(true)
       } else {
         setError("인증 코드 전송에 실패했습니다.")
