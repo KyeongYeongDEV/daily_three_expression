000bf53316056c1fa1db9ecdba2297e764ccad00
최경영 <yeong7098@naver.com>
2025-06-19 10:31:34 +0900

    refactor : 매일 15시에 표현을 생성해 저장하는 배치 등록

--- back/server/src/batch/scheduler/batch.scheduler.ts
+++ back/server/src/batch/scheduler/batch.scheduler.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { BatchMailService } from '../service/batch.service';
-import { ExpressionGenerationService } from 'src/expression/service/expression-generation.service';
+import { AiService } from 'src/ai/service/ai.service';
 
 @Injectable()
 export class BatchMailScheduler {
   constructor(
     private readonly batchService: BatchMailService,
-    private readonly expressionGenerator: ExpressionGenerationService,
+    private readonly aiService : AiService,
   ) {}
 
   //@Cron('*/2 * * * *') // 매 2분마다 실행 (테스트용)
   @Cron('0 6 * * 1-5') // 평일(월~금) 새벽 6시마다
-  async handleCron() {
+  async sendExpressionByEmail() {
+    console.log('✅ Batch Send Expression started');
     await this.batchService.sendEmails();
   }
 
-  // @Cron('0/2 * * * *') //  @Cron('* 3 * * * *')매일 새벽 3시
-  // async handle() {
-  //   console.log('✅ Batch Expression Generation started');
-  //   await this.expressionGenerator.runExpressionGenerationBatch();
-  // }
+  @Cron('0 15 * * *') // 매일 오후 3시에 실행
+  async getExpressionFromAi() {
+    console.log('✅ Batch Get Expression From AI started');
+    await this.aiService.generateAndSaveUniqueExpressions();
+  }
 }
