94ce499eb2bcd0473dc5178bc284725891103fa1
최경영 <yeong7098@naver.com>
2025-07-01 00:31:25 +0900

    refactor : 표현 생성 매일 10,22시에 실행되도록 수정

--- back/server/src/batch/scheduler/batch.scheduler.ts
+++ back/server/src/batch/scheduler/batch.scheduler.ts
@@ -11,7 +11,7 @@ export class BatchMailScheduler {
   ) {}
 
   //@Cron('*/2 * * * *') // 매 2분마다 실행 (테스트용)
-  @Cron('0 6 * * 1-5') // 평일(월~금) 새벽 6시마다
+  @Cron('0 10,22 * * *') // 매일 10시, 22시에 실행
   async sendExpressionByEmail() {
     console.log('✅ Batch Send Expression started');
     await this.batchService.sendEmails();
